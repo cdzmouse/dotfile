684fdd3b4cd12eee53f3838dee64abc7e254f20c
cdzmouse@qq.com <cdzmouse@qq.com>
2016-04-14 16:10:37 +0800

    增加ack版本控制

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@
 tar xvf vim_plugin.tgz
 
 yum install ack -y
-
+yum install -y cgdb
+rpm -i http://dl.fedoraproject.org/pub/epel/6/x86_64/cgdb-0.6.7-1.el6.x86_64.rpm
 
 ##安装vim配置
 ln -fs `pwd`/vim    ~/.vim

01332a128c413ef0f64a2c95ae1bd9134e64ed72
cdzmouse@qq.com <cdzmouse@qq.com>
2016-07-25 14:13:11 +0800

    ack cgdb install bug

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 
 tar xvf vim_plugin.tgz
 
-yum install ack -y
-yum install -y cgdb
+#yum install ack -y
+wget http://beyondgrep.com/ack-2.12-single-file && sudo mv ack-2.12-single-file /usr/local/bin/ack &&  sudo chmod 0755 /usr/local/bin/ack
+
+yum install -y gdb cgdb
 rpm -i http://dl.fedoraproject.org/pub/epel/6/x86_64/cgdb-0.6.7-1.el6.x86_64.rpm
+
 
 ##安装vim配置
 ln -fs `pwd`/vim    ~/.vim
